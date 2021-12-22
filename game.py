e89dfdd14d7c91d932fd8aa5a7f64767215e7eca
Fengwei Yu <fengwei.yu@lewagon.com>
2021-12-22 12:15:09 +0100

    up

--- game.py
+++ game.py
@@ -1,6 +1,22 @@
 # game.py
 
 # [...]
+import requests
+
+class Game:
+    # [...]
+
+    def is_valid(self, word):
+        # [...]
+
+        return self.__check_dictionary(word)
+
+
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://wagon-dictionary.herokuapp.com/{word}")
+        json_response = response.json()
+        return json_response['found']
 
 def is_valid(self, word):
     if not word:
