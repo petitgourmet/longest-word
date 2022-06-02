1204b15df270f382fb0631533eff5d053a6b5978
Fengwei Yu <fengwei.yu@lewagon.com>
2022-06-02 16:20:33 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,27 +1,28 @@
-# game.py
+# pylint: disable=missing-docstring
+# pylint: disable=too-few-public-methods
 
-# [...]
-import requests
+import string
+import random
+
 
 class Game:
-    # [...]
+
+    def __init__(self):
+        self.grid = []
+        for _ in range(9):
+            self.grid.append(random.choice(string.ascii_uppercase))
+        print(self.grid)
+
+    def random_grid(self):
+        pass
 
     def is_valid(self, word):
         if not word:
             return False
-        letters = self.grid.copy() # Consume letters from the grid
+        letters = self.grid.copy()  # Consume letters from the grid
         for letter in word:
             if letter in letters:
                 letters.remove(letter)
             else:
                 return False
         return True
-
-        return self.__check_dictionary(word)
-
-
-    @staticmethod
-    def __check_dictionary(word):
-        response = requests.get(f"https://wagon-dictionary.herokuapp.com/{word}")
-        json_response = response.json()
-        return json_response['found']
