048e043c29a91fdf5c90195073938bce41496672
Fengwei Yu <fengwei.yu@lewagon.com>
2022-06-03 10:51:02 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 # wsgi.py
 # pylint: disable=missing-docstring
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from game import Game
 
 app = Flask(__name__)
@@ -10,3 +10,16 @@ app = Flask(__name__)
 def home():
     game = Game()
     return render_template('home.html', grid=game.grid)
+
+
+@app.route('/check', methods=["POST"])
+def check():
+    game = Game()
+    game.grid = list(request.form['grid'])
+    # the key/value pairs in the body, from a HTML post form, or JavaScript request that isn't JSON encoded
+    word = request.form['word']
+    is_valid = game.is_valid(word)
+    return render_template('check.html',
+                           is_valid=is_valid,
+                           grid=game.grid,
+                           word=word)
